7be0f0c375b3faf9134c3337a77819ef173893ea
alex <keleralexei@gmail.com>
2026-05-12 19:27:06 +0200

    Small changes and final commit before submission on ILIAS.

--- ex01/HWP_Sheet_01_Task1_Template/HWP_Sheet_01_Task1_Template.ino
+++ ex01/HWP_Sheet_01_Task1_Template/HWP_Sheet_01_Task1_Template.ino
@@ -9,6 +9,9 @@
 // ------------------------------------------------------------
 unsigned long last_blink_ms = 0;  // timestamp of the last LED toggle
 bool          led_on        = false; // current LED state
+int cycle_count = 0;
+int colour = 0;
+int LEDs[] = {LED_RED, LED_GREEN, LED_BLUE};
 
 // TODO (Bonus): add a counter for completed blink cycles
 //               and a variable to track the current colour
@@ -20,6 +23,12 @@ void setup() {
   // Set LED pins as outputs.
   // pinMode(pin, OUTPUT) configures a pin for digital output
   // Start with all LEDs off (active-low → HIGH = off)
+  pinMode(LED_RED, OUTPUT);
+  digitalWrite(LED_RED, HIGH);
+  pinMode(LED_GREEN, OUTPUT);
+  digitalWrite(LED_GREEN, HIGH);
+  pinMode(LED_BLUE, OUTPUT);
+  digitalWrite(LED_BLUE, HIGH);
 }
 
 
@@ -35,6 +44,11 @@ void loop() {
   //       turn LED_RED off,
   //       wait BLINK_INTERVAL_MS ms.
 
+// MEIN CODE:
+/* digitalWrite(LED_RED, LOW);
+delay(BLINK_INTERVAL_MS);
+digitalWrite(LED_RED, HIGH);
+delay(BLINK_INTERVAL_MS); */
 
   // ----------------------------------------------------------
   //  Task 1 ii) — Blink using millis()  (comment out i) first)
@@ -44,10 +58,26 @@ void loop() {
   // TODO: if yes — update last_blink_ms, toggle led_on,
   //               and write the correct HIGH/LOW to LED_RED.
 
+/*if (millis() - last_blink_ms  >= BLINK_INTERVAL_MS){
+  last_blink_ms = millis();
 
+  if (!led_on){
+    digitalWrite(LED_RED, LOW);
+    led_on = true;
+  }
+  else {
+    digitalWrite(LED_RED, HIGH);
+    led_on = false;
+  }
+}
+*/
   // ----------------------------------------------------------
   //  Task 1 iii) — Answer as a comment
   // ----------------------------------------------------------
+
+// Our answer:
+// delay() blocks the CPU while millis() does not block the program.  Therefore the advantage is
+// that in case of millis() the program can continue to simultaneously perform other tasks.
 
   // ----------------------------------------------------------
   //  Bonus — Colour cycling after 10 blink cycles
@@ -57,4 +87,23 @@ void loop() {
   //         red → green → blue → red → ...
   //       Turn off all LEDs before switching, then turn on only
   //       the new active colour.
+
+  if (millis() - last_blink_ms  >= BLINK_INTERVAL_MS){
+    last_blink_ms = millis();
+
+    if (!led_on){
+      digitalWrite(LEDs[colour], LOW);
+      led_on = true;
+    }
+    else {
+      digitalWrite(LEDs[colour], HIGH);
+      led_on = false;
+      ++cycle_count;
+    }
+    if (cycle_count == 10){
+      cycle_count = 0;
+      colour = (colour + 1) % 3;
+    }
+  }
+
 }
