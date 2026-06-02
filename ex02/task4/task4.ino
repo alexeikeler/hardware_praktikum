645f1feffa2db68a1318c0157cbfa0a6d1d6f123
corinna <preuss.corinna.cp@gmail.com>
2026-06-03 01:03:08 +0200

    Task 4 solution

--- ex02/task4/task4.ino
+++ ex02/task4/task4.ino
@@ -6,11 +6,17 @@
 // ------------------------------------------------------------
 
 #include <Arduino.h>
-
+volatile bool buzzerHigh = false;
+volatile bool buzzerEnabled = false;
 
 void setup() {
-
-
+  NRF_P0->DIRSET = (1UL << 29); // Set P0.29 as output
+  NRF_TIMER1->MODE = 0;
+  NRF_TIMER1->BITMODE = 3;
+  NRF_TIMER1->PRESCALER = 4;
+  NRF_TIMER1->SHORTS = (1UL << 0);
+  NRF_TIMER1->INTENSET = (1UL << 16);
+  NVIC_EnableIRQ(TIMER1_IRQn);
 }
 
 
@@ -33,10 +39,31 @@ void loop() {
 
 
 void setBuzzerFreq(uint32_t freq) {
+  if (freq < 100 || freq > 3000){
+    NRF_TIMER1->TASKS_STOP = 1;
+    NRF_P0->OUTCLR = (1UL << 29);
+    buzzerHigh = false;
+    buzzerEnabled = false;
+    return;
+  }
+  NRF_TIMER1->CC[0] = 1000000/(2 * freq);
+
+  NRF_TIMER1->TASKS_CLEAR = 1;
+  NRF_TIMER1->TASKS_START = 1;
 
 }
 
 
 extern "C" void TIMER1_IRQHandler() {
-
+  if (NRF_TIMER1->EVENTS_COMPARE[0]){
+    NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+    if (buzzerHigh){
+    NRF_P0->OUTCLR = (1UL << 29);
+      buzzerHigh = false;
+    }
+    else {
+      NRF_P0->OUTSET = (1UL << 29);
+      buzzerHigh = true;
+    }
+  }
 }
