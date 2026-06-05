0486a594adc36fc7969aecf74fe23afaae474b63
corinna <preuss.corinna.cp@gmail.com>
2026-06-05 12:33:38 +0200

    added almost nothing to task 7 but at least not completely nothing

--- ex02/task7/task7.ino
+++ ex02/task7/task7.ino
@@ -63,25 +63,80 @@ uint16_t str2uint(char * buf, uint16_t * idx) {
 
 
 bool isDigit(char c) { 
-
+  return c >= 0 && c<= 9;
 }
 
-
+// from here code from Task 6
 void setBuzzerFreq(uint32_t freq) {
+  if (freq < 100 || freq > 3000){
+    NRF_TIMER1->TASKS_STOP = 1;
+    NRF_P0->OUTCLR = (1UL << 29);
+    buzzerHigh = false;
+    return;
+  }
+  NRF_TIMER1->CC[0] = 1000000/(2 * freq);
+
+  NRF_TIMER1->TASKS_CLEAR = 1;
+
+  NRF_TIMER1->MODE = 0;
+  NRF_TIMER1->BITMODE = 3;
+  NRF_TIMER1->PRESCALER = 4;
+  NRF_TIMER1->SHORTS = (1UL << 0);
+  NRF_TIMER1->INTENSET = (1UL << 16);
+  NVIC_EnableIRQ(TIMER1_IRQn);
+  NRF_TIMER1->TASKS_START = 1;
 
 }
 
 
 extern "C" void TIMER1_IRQHandler() {
-
+  if (NRF_TIMER1->EVENTS_COMPARE[0]){
+    NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+    if (buzzerHigh){
+    NRF_P0->OUTCLR = (1UL << 29);
+      buzzerHigh = false;
+    }
+    else {
+      NRF_P0->OUTSET = (1UL << 29);
+      buzzerHigh = true;
+    }
+  }
 }
 
 
 void setTimer2(bool enable) {
-
+  if(enable){
+  NRF_TIMER2->TASKS_STOP = 1;
+  NRF_TIMER2->TASKS_CLEAR = 1;
+  NRF_TIMER2->MODE = 0;
+  NRF_TIMER2->BITMODE = 3;
+  NRF_TIMER2->PRESCALER = 4;
+  NRF_TIMER2->CC[0] = 1000; 
+  NRF_TIMER2->SHORTS = (1UL << 0);
+  NRF_TIMER2->INTENSET = (1UL << 16); 
+  NVIC_EnableIRQ(TIMER2_IRQn);
+  NRF_TIMER2->TASKS_START = 1;
+  }
+  else{
+    NRF_TIMER2->TASKS_STOP = 1;
+    NVIC_DisableIRQ(TIMER2_IRQn);
+  }
 }
 
-
 extern "C" void TIMER2_IRQHandler() {
-
+  if (NRF_TIMER2->EVENTS_COMPARE[0]){
+    NRF_TIMER2->EVENTS_COMPARE[0] = 0;
+    tCount++;
+    if(tCount >= durations[melodyIdx]){
+      tCount = 0;
+      melodyIdx++;
+      if(melodyIdx >= 10){
+        setTimer2(false);
+        setBuzzerFreq(0); // out of range, silences buzzer
+      }
+      else{
+        setBuzzerFreq(notes[melodyIdx]);
+      }
+    }
+  }
 }
