27ff3a431002a2408f535c0405d492de6224ee64
corinna <preuss.corinna.cp@gmail.com>
2026-06-11 14:27:14 +0200

    cleande task 1 and 2

--- ex03/task1/task1.ino
+++ ex03/task1/task1.ino
@@ -2,15 +2,20 @@
 // Complete the implementation according to the task description.
 #include <Arduino.h>
 #include <Adafruit_TinyUSB.h>
-#define BLINK_INTERVAL_MS  5000UL 
-// TODO: define necessary constants (e.g., calibration range, thresholds, timing)
+#define SAMPLE_INTERVAL_MS  5000UL 
+// define necessary constants (e.g., calibration range, thresholds, timing)
 int calibrationlow = 50;
 int calibrationhigh = 3500;
 
-unsigned long last_blink_ms = 0;
+// scale constants
+int scaleLow = 0;
+int scaleHigh = 100;
+
+// counter for the interval
+unsigned long last_count_ms = 0;
 
 
-// TODO: implement a function that categorizes the normalized value
+// function that categorizes the normalized value
 const char* categorize(int normalized) {
     if(normalized < 30){
         return "LOW";
@@ -24,31 +29,34 @@ const char* categorize(int normalized) {
 }
 
 void setup() {
-    // TODO: initialize Serial communication
+    // initialize Serial communication
     Serial.begin(115200);
-    // TODO: configure ADC resolution to 12-bit
+    //configure ADC resolution to 12-bit
     analogReadResolution(12);
-
-
 }
 
 void loop() {
-    // TODO: implement non-blocking timing using millis()
-    // The system should sample every 500 ms
-    if (millis() - last_blink_ms  >= BLINK_INTERVAL_MS){
-    last_blink_ms = millis();
-    // TODO: read raw value from light sensor
+    // The system samples every 500 ms
+    if (millis() - last_count_ms  >= SAMPLE_INTERVAL_MS){
+    last_count_ms = millis();
+
+    // read raw value from light sensor
     int lightVal = analogRead(A0);
-    // TODO: clamp the raw value to a calibrated range
+    // clamp the raw value to a calibrated range
     int constrainVal = constrain(lightVal, calibrationlow, calibrationhigh);
-    // TODO: normalize the value to a 0–100 scale
-    int normVal = map(constrainVal, calibrationlow,calibrationhigh, 0, 100);
-    // TODO: determine the category using the categorize() function
+    // normalize the value to a 0–100 scale
+    int normVal = map(constrainVal, calibrationlow,calibrationhigh, scaleLow, scaleHigh);
+    // determine the category using the categorize() function
     const char* category = categorize(normVal);
-    // TODO: print raw value, normalized value, and category to Serial
+
+    // print raw value, normalized value, and category to Serial
+    Serial.print("raw Value:  ");
     Serial.println(lightVal);
+    Serial.print("normalized Value:  ");
     Serial.println(normVal);
+    Serial.print("category:  ");
     Serial.println(category);
+
     }
 
 }
--- ex03/task2/task2.ino
+++ ex03/task2/task2.ino
@@ -4,24 +4,24 @@
 #include <math.h>
 
 // --- Configuration ---
-// TODO: define sensor pin and type (DHT11)
+// define sensor pin and type (DHT11)
 #define DHTTYPE DHT11
-#define DHTPIN D7 // D7 didn't work, only got nan's
+#define DHTPIN D7
 
 // --- Objects ---
-// TODO: create DHT sensor instance
+// create DHT sensor instance
 DHT dht(DHTPIN, DHTTYPE);
 // --- Timing ---
 unsigned long lastSample = 0;
-// TODO: define sampling interval (2 seconds)
+//  sampling interval (2 seconds)
 #define intervalms 2000
 int last_blink_ms = 0;
 
 // --- State Variables ---
-// TODO: store last valid temperature and humidity
+// store last valid temperature and humidity
 float lastValidTemp;
 float lastValidHumid;
-// TODO: maintain a failure counter
+// maintain a failure counter
 float failure_counter = 0;
 
 
@@ -30,7 +30,7 @@ float b = 243.12;
 // --- Computation ---
 
 float computeDewPoint(float tempC, float relHum) {
-    // TODO: implement Ma`gnus formula using natural logarithm
+    //  Magnus formula using natural logarithm
     float gamma = log(relHum / 100) + (b * tempC) / (a + tempC);
     return (a * gamma) / (b - gamma);
 }
@@ -39,7 +39,6 @@ void setup() {
     Serial.begin(115200);
     while (!Serial && millis() < 3000);
 
-    // TODO: initialize sensor
 }
 
 void loop() {
@@ -51,11 +50,12 @@ void loop() {
     // TODO: read temperature and humidity
     float hum = dht.readHumidity();
     float temp = dht.readTemperature();
+
     if (isnan(hum)){ 
         failure_counter +=1;
     }
     else{
-        lastValidHumid = hum;
+        lastValidHumid = hum; // not nan hum is valid
         failure_counter = 0;
     }
 
@@ -63,7 +63,7 @@ void loop() {
         failure_counter +=1;
         }
     else{
-        lastValidTemp = temp;
+        lastValidTemp = temp; // not nan to temp is valid
         failure_counter = 0;
     }
 
@@ -86,12 +86,6 @@ void loop() {
         Serial.print(failure_counter);
     }
 
-
-
     }
-
-
-    // TODO: compute dew point, print formatted output. If failure count exceeds threshold print a warning. 
-    
     
 }
